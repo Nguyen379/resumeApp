cb4ea8b58c51782c8a0d6f77df2bcc5bc58efa2a
nguyen <anhnguyenle379@gmail.com>
2023-06-13 23:38:22 -0500

    logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Le Nguyen',
+        short_name: 'Le Nguyen',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
