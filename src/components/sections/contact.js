be250a6f0bc4903ddc859be0ff710b96ce4f1e87
nguyen <anhnguyenle379@gmail.com>
2023-06-15 02:36:08 -0500

    asdf

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,12 +60,11 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+        I am currently looking for internships so feel free to reach out to me via email/linkedin!
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
-        Say Hello
+        Reach me!
       </a>
     </StyledContactSection>
   );
